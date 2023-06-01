6d27caa887b21abc5bb514643abdb31681d23ddb
Shuai Shao <shshao@nuro.ai>
2023-05-31 22:19:50 -0700

    lint

--- tf_trt_general.py
+++ tf_trt_general.py
@@ -4,7 +4,6 @@ nsys profile --force-overwrite true -w true -t cublas,cuda,nvtx,osrt -s cpu -o /
 
 import os
 import time
-import tempfile
 
 import numpy as np
 import tensorflow as tf
@@ -15,7 +14,8 @@ from tensorflow.python.saved_model import signature_constants
 from tensorflow.compiler.tf2tensorrt.utils import trt_engine_instance_pb2
 
 INPUT_SAVED_MODEL_DIR = "/tmp/convnet"
-OUTPUT_SAVED_MODEL_DIR = "/tmp/convnet_trt"
+OUTPUT_SAVED_MODEL_DIR = INPUT_SAVED_MODEL_DIR + "_trt"
+TRT_ENGINE_DIR = OUTPUT_SAVED_MODEL_DIR + "_engine"
 
 # build input fn
 saved_model = tf.saved_model.load(INPUT_SAVED_MODEL_DIR)
@@ -29,7 +29,8 @@ def freeze_shape(shape):
 
 def input_fn():
     yield [
-        tf.zeros(shape=freeze_shape(input_tensor.shape), dtype=input_tensor.dtype)
+        tf.zeros(shape=freeze_shape(input_tensor.shape),
+                 dtype=input_tensor.dtype)
         for input_tensor in frozen_func.inputs
     ]
 
@@ -46,8 +47,10 @@ saved_model_loaded = tf.saved_model.load(
 graph_func = saved_model_loaded.signatures[
     signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
 
+
 def spec_to_tensor(spec): return tf.zeros(
     dtype=spec.dtype, shape=freeze_shape(spec.shape))
+
 
 arg_specs, kwarg_specs = graph_func.structured_input_signature
 args = [spec_to_tensor(arg_spec) for arg_spec in arg_specs]
@@ -60,15 +63,17 @@ output = graph_func(*args, **kwarg_specs)
 print("trt time", time.time() - start)
 
 # trtexec
-with tempfile.TemporaryDirectory() as tmp_dir:
-    dataset = tf.data.TFRecordDataset(os.path.join(OUTPUT_SAVED_MODEL_DIR, "assets", "trt-serialized-engine.TRTEngineOp_000_000"))
-    serialized_proto_list = list(iter(dataset))
-    assert len(serialized_proto_list) == 1
-    engine_instance = trt_engine_instance_pb2.TRTEngineInstance()
-    engine_instance.ParseFromString(serialized_proto_list[0].numpy())
-    raw_engine_path = os.path.join(tmp_dir, "raw_engine")
+dataset = tf.data.TFRecordDataset(os.path.join(
+    OUTPUT_SAVED_MODEL_DIR, "assets", "trt-serialized-engine.TRTEngineOp_000_000"))
+serialized_proto_list = list(iter(dataset))
+assert len(serialized_proto_list) == 1
+engine_instance = trt_engine_instance_pb2.TRTEngineInstance()
+engine_instance.ParseFromString(serialized_proto_list[0].numpy())
+raw_engine_path = os.path.join(TRT_ENGINE_DIR, "raw_engine")
+if not os.path.exists(TRT_ENGINE_DIR):
+    os.mkdir(TRT_ENGINE_DIR)
 
-    with open(raw_engine_path, "wb") as f:
-        f.write(engine_instance.serialized_engine)
+with open(raw_engine_path, "wb") as f:
+    f.write(engine_instance.serialized_engine)
 
-    os.system(f"trtexec --loadEngine={raw_engine_path}")
+os.system(f"trtexec --loadEngine={raw_engine_path} --dumpProfile")
