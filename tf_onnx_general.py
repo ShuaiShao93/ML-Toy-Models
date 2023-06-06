7ffdc1f118715e33fcdd46e3b5e9505687f06f01
Shuai Shao <shshao@nuro.ai>
2023-06-05 18:14:08 -0700

    inspect onnx

--- tf_onnx_general.py
+++ tf_onnx_general.py
@@ -1,10 +1,18 @@
 import os
+import onnx
 
 INPUT_SAVED_MODEL_DIR = "/tmp/toy"
 ONNX_MODEL_PATH = INPUT_SAVED_MODEL_DIR + ".onnx"
 
-
+# convert to onnx
 os.system(
     f"python -m tf2onnx.convert --saved-model {INPUT_SAVED_MODEL_DIR} --output {ONNX_MODEL_PATH}")
 
-# os.system(f"trtexec --onnx={ONNX_MODEL_PATH} --dumpProfile")
+# inspect onnx graph/node
+onnx_model = onnx.load(ONNX_MODEL_PATH)
+for node in onnx_model.graph.node:
+    if node.name == "NODE NAME":
+        print(node)
+
+# convert to trt
+os.system(f"trtexec --onnx={ONNX_MODEL_PATH} --dumpProfile")
