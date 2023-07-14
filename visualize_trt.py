a7f6d2decbe451a4f36aeb6ed9c5d752a91488fa
Shuai Shao <shshao@nuro.ai>
2023-07-14 12:02:24 -0700

    visualize trt

---
+++ visualize_trt.py
@@ -0,0 +1,23 @@
+# cd TensorRT/tools/experimental/trt-engine-explorer
+# python3 -m pip install -e .
+import os
+import trex
+
+MODEL_NAME = "ResNet18"
+MODEL_PATH = f"/tmp/{MODEL_NAME}.onnx"
+
+cmd = "python3 TensorRT/tools/experimental/trt-engine-explorer/utils/process_engine.py"
+cmd += f" {MODEL_PATH} /tmp/{MODEL_NAME}_trex/ fp16"
+os.system(cmd)
+
+engine_name = f"/tmp/{MODEL_NAME}_trex/{MODEL_NAME}.onnx.engine"
+
+plan = trex.EnginePlan(
+    f"{engine_name}.graph.json",
+    f"{engine_name}.profile.json",
+    f"{engine_name}.profile.metadata.json")
+
+plan.summary()
+
+graph = trex.to_dot(plan, trex.layer_type_formatter)
+svg_name = trex.render_dot(graph, engine_name, 'svg')
